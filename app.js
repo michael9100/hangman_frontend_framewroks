d911b3af5dddad9eb6d2718df72f9f6c32b94537
Michael Web Cork <m.oconnell56@yahoo.ie>
2018-09-17 20:45:41 +0100

    set up basic api

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
 const app = express()
-const port = 3000
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/posts', (req, res) => {
+  res.send(
+    [{
+      title: "Hello World!",
+      description: "Hi there! How are you?"
+    }]
+  )
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(3000)
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
+import Posts from '@/views/Posts.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -20,6 +22,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {
+      path: '/posts',
+      name: 'Posts',
+      component: Posts
+    },
   ]
 })
