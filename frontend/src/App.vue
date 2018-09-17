2115db2819099ac9656a4712d1998f7bd5ebe509
MichaelWebCork <m.oconnell56@yahoo.ie>
2018-09-17 16:43:26 +0100

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div>
+      <b-btn v-b-modal.modal1>Launch demo modal</b-btn>
+
+      <!-- Modal Component -->
+      <b-modal id="modal1" title="Bootstrap-Vue">
+        <p class="my-4">Hello from modal!</p>
+      </b-modal>
+    </div>
+
+
+
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
