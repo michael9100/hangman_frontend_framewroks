8276a01d66ca2d516a8c6d316062e812300c4a87
MichaelWebCork <m.oconnell56@yahoo.ie>
2018-09-17 17:50:47 +0100

    addedexpress js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
     <div>
-      <b-btn v-b-modal.modal1>Launch demo modal</b-btn>
-
-      <!-- Modal Component -->
-      <b-modal id="modal1" title="Bootstrap-Vue">
-        <p class="my-4">Hello from modal!</p>
-      </b-modal>
+      <b-navbar  type="light" variant="light" toggleable>
+        <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
+        <b-collapse is-nav id="nav_dropdown_collapse">
+          <b-navbar-nav>
+            <b-navbar-brand to="/">Hangman</b-navbar-brand>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-button to="/register" variant="link">Register</b-button>
+            <b-button to="/login" variant="link">Login</b-button>
+            <b-button to="/logout" variant="link">Logout</b-button>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
-
-
-
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
