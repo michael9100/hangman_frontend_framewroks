5a0a6d7016c771eb58dfe14eb816892edc9e6407
Michael Web Cork <m.oconnell56@yahoo.ie>
2018-09-18 22:12:24 +0100

    added passport and mysql

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'hangman'
+})
 
 const app = express()
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+
+// =========================================================
+// Database
+// =========================================================
+connection.connect()
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+// connection.end()
 
 app.get('/posts', (req, res) => {
   res.send(
@@ -20,9 +39,9 @@ app.get('/posts', (req, res) => {
 // =========================================================
 // Authentication
 // =========================================================
-app.post('/auth', (req, res) => {
-  console.log("Request: ", req.body)
-  res.send(req.body)
-})
+// app.post('/auth', (req, res) => {
+//   console.log("Request: ", req.body)
+//   res.send(req.body)
+// })
 
 app.listen(3000)
